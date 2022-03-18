47ba3ef03eefb62ab810f8af30489f8f8ed48833
Tom Takeuchi <tt35@students.calvin.edu>
2022-03-17 22:13:06 -0400

    test2

--- lmstt/tests/testthat/test-df_apply.R
+++ lmstt/tests/testthat/test-df_apply.R
@@ -8,9 +8,14 @@ TestData <-
           logical = rep(c(TRUE, FALSE), length.out = n))
 
 
-testthat::test_that("It successfully apply round() to numeric values.", {
+testthat::test_that("It successfully apply round(digit = 0) to numeric values.", {
   testthat::expect_equal(TestData |> df_apply(round, is.numeric, return, digit = 0), 
                TestData |> dplyr::mutate(double = c(2, 2, 5, 9, 10, 1, 4),
                                          x = c(12340, 1234, 123, 12, 1, 0, 0)))
 })
 
+testthat::test_that("It successfully apply round(digit = 2) to numeric values.", {
+  testthat::expect_equal(TestData |> df_apply(round, is.numeric, return, digit = 2), 
+                         TestData |> dplyr::mutate(double = c(1.5, 2.3, 4.6, 8.8, 10.4, 1.3, 4.3),
+                                                   x = c(12340, 1234, 123.40, 12.34, 1.23, 0.12, 0.01)))
+})
