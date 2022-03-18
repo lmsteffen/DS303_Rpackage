ecc2786ef8318d877785213e183a2271e6d3771e
Lauren Steffen <lmsteffen878@gmail.com>
2022-03-17 22:00:51 -0400

    roxygen for oilSpill data

---
+++ lmstt/R/oilSpills.R
@@ -0,0 +1,18 @@
+#' Petroleum Oil Spills Impacting Navigable US Waters
+#'
+#' A dataset containing yearly gallons spilled, number of incidents, and source type
+#' of oil spills from 1985, 1990, 1995-2020.
+#'
+#'@format A data frame with 280 rows and 4 variables:
+#' \describe{
+#'   \item{Year}{year of spill}
+#'   \item{Source}{source of spill}
+#'   \item{Incidents}{number of incidents of the source for that year}
+#'   \item{Gallons spilled}{number of gallons of oil spilled of that source for that year}
+#'
+#'@examples 
+#' oilSpills %>% group_by(Year) %>% summarize(Gallons = sum(`Gallons spilled`))
+#' 
+#'
+#'@source \url{https://www.bts.gov/content/petroleum-oil-spills-impacting-navigable-us-waters}
+#'
