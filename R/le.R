351fdd395b4a2aceabc994f29355ca5e136374f5
Tom Takeuchi <tt35@students.calvin.edu>
2022-03-18 16:55:36 -0400

    i dont know what it is doing

--- R/le.R
+++ R/le.R
@@ -4,18 +4,17 @@
 #' GDP per capita, and Population for year from 1950 to 2019
 #'
 #' @format A data frame with 60067 rows and 7 variables:
-#' \describe{
-#'   \item{Entity}{Country names}
-#'   \item{Code}{Country code (AFG for Afghanistan)}
-#'   \item{Year}{Year when the data was collected}
-#'   \item{Life expectancy}{Life expectancy}
-#'   \item{GDP per capita}{GDP per capita}
-#'   \item{145446-annotations}{not sure... should be ignored}
-#'   \item{population_historical_estimates}{Population}
-#'   \item{continent}{Continent of which the country belongs to}
-#'   }
-#' @examples
-#' mean_life_expectancy <- mean(le$life_expectancy)
+#' *Entity Country names
+#' *Code Country code (AFG for Afghanistan)
+#' *Year when the data was collected
+#' *Life_expectancy Life expectancy
+#' *GDP_per_capita GDP per capita
+#' *145446-annotations not sure... should be ignored
+#' *population_historical_estimates Population
+#' *continent Continent of which the country belongs to
+#'
+#' @examples mean_life_expectancy <- mean(le$life_expectancy)
+#' 
 #' @source \url{https://ourworldindata.org/grapher/life-expectancy-vs-gdp-per-capita"
-"le"
 
+"le"
